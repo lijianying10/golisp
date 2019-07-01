7ef03bbb79dac68ce7bd2363cbe9bb27948f1dcd
jianying.li <lijianying12@gmail.com>
2019-07-01 22:16:29 +0800

    read signal

--- go/runtime/rt.go
+++ go/runtime/rt.go
@@ -4,12 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/signal"
 )
 
 func READ() string{
 	fmt.Print("golisp> ")
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
+	if len(text)==0{
+		fmt.Println("READ EOF exit!")
+		os.Exit(0)
+	}
 	return text
 }
 
@@ -21,7 +26,21 @@ func PRINT(source string){
 	fmt.Print(source)
 }
 
+func SIGNALHandler(){
+	c := make(chan os.Signal, 1)
+
+	// Passing no signals to Notify means that
+	// all signals will be sent to the channel.
+	signal.Notify(c)
+
+	// Block until any signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+	os.Exit(0)
+}
+
 func GOLISP(){
+	go SIGNALHandler()
 	for {
 		PRINT(EVAL(READ()))
 	}
